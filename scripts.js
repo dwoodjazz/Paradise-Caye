e746f1774a40f50684587060599efb4deed3e128
Douglas Wood <douglaswood@Douglass-MacBook-Pro.local>
2022-01-27 05:29:37 -0800

    first upload all files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// const party = () =>{
+//     let x = document.getElementById("title");
+//     x.innerHTML="heeby-jeeby";
+// }
+
+// party();
+
+document.getElementById("title").innerHTML="the external file finally works";
